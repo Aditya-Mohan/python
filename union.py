725d7fd88589aa40aee34c23ff08d04af0e7afdc
Aditya Mohan <73834884+Aditya-Mohan@users.noreply.github.com>
2022-09-29 12:21:09 +0530

    Add files via upload

---
+++ union.py
@@ -0,0 +1,5 @@
+Japanese = {"Honda", "Nissan", "Mitsubishi"}
+European = {"Volvo", "Bugatti", "Ferrari"}
+American = {"Ford", "Chevrolet", "Cadillac"}
+z = American.union(European, Japanese)
+print(z)
