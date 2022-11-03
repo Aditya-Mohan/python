2b485de08e663b39be4fde6ae3e925b3bba3976e
Aditya Mohan <73834884+Aditya-Mohan@users.noreply.github.com>
2022-11-03 18:21:05 +0530

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,18 @@
+'''def square_num(value):
+    squared_value = value*value
+    return squared_value
+
+
+returned_value = square_num(int(input()))
+print(returned_value)'''
+
+
+def even_or_odd(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+returned_value = even_or_odd(int(input()))
+print(returned_value)
